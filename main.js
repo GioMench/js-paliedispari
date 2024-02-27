6c707ced13592e5736a0edc08d7629cb185a387e
GioMench <giorgiaglm@gmail.com>
2024-02-27 18:19:06 +0100

    snack-pari&dispari

--- main.js
+++ main.js
@@ -61,67 +61,47 @@ if (userWord == reversWord(userWord)) {
 
 console.log('SNACK PARI E DISPARI');
 
-// L’utente sceglie pari o dispari
-do {
-    document.getElementById("pari").addEventListener("click", function numberType() {
 
-        //let possibleNumb = [1, 2, 3, 4, 5];
-        // e inserisce un numero da 1 a 5.
-        let userNumber = Number(prompt('scrivi un numero da 1 a 5'));
-        console.log(userNumber);
+// sceglie un numero tra 1 e 5
+/**
+ * richiedo integer number
+ * integer Number da verificare se inclusa in array
+ * return a valid userNumber
+ * 
+ */
 
-        /* for (let i = 0; i < possibleNumb.length; i++) {
-             let listNumb = possibleNumb[i];
- 
-             if (userNumber !== listNumb) {
- 
-                 alert('ATTENZIONE, scrivi un numero da 1 a 5');
-                 break;
-             }
-             else  {
- 
-                     let pcNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-                     console.log(pcNumber);
- 
-                 }
-                }*/
+function askUserNumber() {
 
-        // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    let possibleNumb = [1, 2, 3, 4, 5];
 
-        //let pcNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-        
-
-        function createNumbPc() {
-
-            let pcNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
-            return pcNumber;
-            
-        }
-        console.log(createNumbPc());
-
-        let sum = (userNumber + createNumbPc());
-        console.log(sum);
-
-
-
-
-    });
-} while (document.getElementById("dispari").addEventListener("click", function numberType() {
-
-    let userNumber = prompt('scrivi un numero da 1 a 5')
+    let userNumber = Number(prompt('scrivi un numero da 1 a 5'));
     console.log(userNumber);
-})
-)
 
+    for (let i = 0; i < possibleNumb.length; i++) {
 
+        if (!possibleNumb.includes(userNumber)) {
+            userNumber = Number(prompt('ATTENZIONE, scrivi un numero da 1 a 5'));
+            console.log(userNumber);
 
+        } else {
+            break;
+        }
 
+    }
 
+    return userNumber;
+}
 
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+/**
+     * Ogni volta che chiamo la funzione mi genera e restituisce un numero casuale quindi do alla funzione una costante in addEventListener
+     * return a random number compreso tra 1 e 5 
+     */
+function createNumbPc() {
+    let pcNumber = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+    return pcNumber;
 
-
-
-
+}
 
 
 // Sommiamo i due numeri
@@ -129,4 +109,87 @@ do {
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 
 // Dichiariamo chi ha vinto.
+/**
+ * 
+ * integer number Numero da verifacare
+ * string odd_or_even Condizione da verifacare 'pari', 'dispari'
+ * 
+ * return boolean True se le condizioni sono vere, false se non si sono verificate
+ */
+function oddEven(number, odd_or_even) {
+
+    if (odd_or_even == 'pari' && number % 2 == 0) {
+        return true;
+    } else if (odd_or_even == 'dispari' && number % 2 != 0) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+// L’utente sceglie pari o dispari
+document.getElementById("pari").addEventListener("click", function () {
+
+    // sceglie un numero tra 1 e 5
+    let userNumber = askUserNumber();
+
+    // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    let pcNumber = createNumbPc();
+    console.log(pcNumber);
+
+
+    // Sommiamo i due numeri
+
+    // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+    // Dichiariamo chi ha vinto.
+    if (oddEven(userNumber + pcNumber, 'pari')) {
+        console.log('you win!');
+    } else {
+        console.log('sorry, you lose');
+    }
+
+
+
+
+});
+
+// L’utente sceglie pari o dispari
+document.getElementById("dispari").addEventListener("click", function () {
+
+    // sceglie un numero tra 1 e 5
+    let userNumber = askUserNumber();
+
+    // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+    let pcNumber = createNumbPc();
+    console.log(pcNumber);
+
+
+    // Sommiamo i due numeri
+
+    // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+    // Dichiariamo chi ha vinto.
+    if (oddEven(userNumber + pcNumber, 'dispari')) {
+        console.log('you win!');
+    } else {
+        console.log('sorry, you lose');
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
